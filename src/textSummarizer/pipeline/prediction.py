796abbcccd52e8bf984d31ca6968e6f6a7cec3e4
mappy92 <manpreet4114@gmail.com>
2026-01-18 18:56:04 -0500

    Model Training complete

--- src/textSummarizer/pipeline/prediction.py
+++ src/textSummarizer/pipeline/prediction.py
@@ -11,7 +11,17 @@ class PredictionPipeline:
     
     def predict(self,text):
         tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
-        gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": 128}
+        # gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": 128}
+        gen_kwargs = {
+        "max_length": 128,
+        "min_length": 30,
+        "num_beams": 6,
+        "length_penalty": 1.0,
+        "early_stopping": True,
+        "repetition_penalty": 2.0,
+        "no_repeat_ngram_size": 3,
+        "do_sample": False
+    }
 
         pipe = pipeline("summarization", model=self.config.model_path,tokenizer=tokenizer)
 
